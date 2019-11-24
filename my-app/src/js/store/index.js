869ff4955c9f07fc7365096ef166b755bdfb522c
ct <ctaylr13@gmail.com>
2019-11-23 20:38:06 -0500

    Update index.js

--- my-app/src/js/store/index.js
+++ my-app/src/js/store/index.js
@@ -3,6 +3,7 @@ import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
 import thunk from "redux-thunk";
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
   storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
